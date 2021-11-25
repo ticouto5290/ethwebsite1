885e027e17af54914c3f766ed3093335ac3b2b9c
ticouto5290 <raousingbertrand@gmail.com>
2021-11-25 11:18:36 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ Hello Beautiful World
 
 it is <br>
 
-<?php
-echo date("d/m/Y h:i:sa");
-?>
+<script>
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+</script>
